5670fe3aafe9d0bfae7418ba91fcda11b0cc4971
HouseAtreides24 <77858384+HouseAtreides24@users.noreply.github.com>
2022-02-06 13:46:30 +0200

    message3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import UserDetails from "./HomeWork2/components/UserDetails/UserDetails";
 import Users from './HomeWork2/components/Users/User'
 import css from './App.module.css'
+import Posts from "./HomeWork2/components/Posts/Posts";
 
 
 function App() {
