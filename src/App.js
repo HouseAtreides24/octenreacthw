0353451098175c61c32da5ad6d8b50a6b584fb3b
HouseAtreides24 <77858384+HouseAtreides24@users.noreply.github.com>
2022-02-06 13:13:45 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import {useEffect, useState} from "react";
 import Users from "./HomeWprk1/componennts/Users";
 import Posts from "./HomeWprk1/componennts/Posts";
 import Comments from "./HomeWprk1/componennts/Comments";
+import UserDetails from "./HomeWork2/components/UserDetails/UserDetails";
+
+import css from './App.module.css'
 
 function App() {
 
   return (
 
   <div >
-    <Users/>
+    {/*<Users/>*/}
+    {/*<Posts/>*/}
+    {/*<Comments/>*/}
+
+    <div className={css.wrap}>
+      <Users/>
+      <UserDetails/>
+    </div>
+
     <Posts/>
-    <Comments/>
+
   </div>
   )
 }
