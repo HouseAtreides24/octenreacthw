4ab70fa0deddbf6116698ef15c4fd28d89e20dcc
HouseAtreides24 <77858384+HouseAtreides24@users.noreply.github.com>
2022-02-06 13:43:49 +0200

    message2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
-import Users from "./HomeWprk1/componennts/Users";
-import Posts from "./HomeWprk1/componennts/Posts";
-import Comments from "./HomeWprk1/componennts/Comments";
-import UserDetails from "./HomeWork2/components/UserDetails/UserDetails";
 
+import './App.css';
+
+import UserDetails from "./HomeWork2/components/UserDetails/UserDetails";
+import Users from './HomeWork2/components/Users/User'
 import css from './App.module.css'
+
 
 function App() {
 
